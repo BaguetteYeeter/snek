56206507215c2c364af1be874803e05235bb0df5
BaguetteYeeter <baguetteyeeter@gmail.com>
2020-12-16 19:26:12 +0000

    i made a square move around

---
+++ main.py
@@ -0,0 +1,54 @@
+import pygame
+import math
+
+cells = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+cellX = [0, 0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 0, 50, 100, 150, 200, 250, 300, 350, 400, 450]
+cellY = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 150, 150, 150, 150, 150, 150, 150, 150, 150, 150, 200, 200, 200, 200, 200, 200, 200, 200, 200, 200, 250, 250, 250, 250, 250, 250, 250, 250, 250, 250, 300, 300, 300, 300, 300, 300, 300, 300, 300, 300, 350, 350, 350, 350, 350, 350, 350, 350, 350, 350, 400, 400, 400, 400, 400, 400, 400, 400, 400, 400, 450, 450, 450, 450, 450, 450, 450, 450, 450, 450]
+currentCell = 1
+prevCell = 0
+
+pygame.init()
+#initialize pygame
+mainScreen = pygame.display.set_mode((500, 500))
+#set display resolution
+
+pygame.display.set_caption("snek")
+
+run = True
+
+while run:
+    pygame.time.delay(90)
+    #set the delay
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    #close the program if the window closes
+
+    mainScreen.fill((0, 0, 0))
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_UP]:
+        currentCell -= 10
+
+    if keys[pygame.K_DOWN]:
+        currentCell += 10
+
+    if keys[pygame.K_LEFT]:
+        currentCell -= 1
+
+    if keys[pygame.K_RIGHT]:
+        currentCell += 1
+
+    cells[currentCell] = 1
+
+    if prevCell != currentCell:
+        cells[prevCell] = 0
+    prevCell = currentCell
+
+    for i in range(1,100):
+        if cells[i] == 1:
+            pygame.draw.rect(mainScreen, (255, 255, 255), (cellX[i], cellY[i], 50, 50))
+
+    pygame.display.update()
